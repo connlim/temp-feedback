2590979632e70989474b9304e0bbe12448f0656f
Connor Lim <dev@connorlim.net>
2021-02-27 21:06:06 +0800

    Add basic javascript for feedback page

--- nextjs/pages/feedback.js
+++ nextjs/pages/feedback.js
@@ -7,12 +7,9 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
-
-function submitFeedback() {
-  alert("Hello world");
-}
 
 function FeedbackCard(props) {
   return (
@@ -27,7 +24,28 @@ function FeedbackCard(props) {
   );
 }
 
-export default function Feedback() {
+export default function Feedback(props) {
+  const [inputText, setInputText] = useState("");
+
+  const updateFeedbackInput = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const submitFeedback = (event) => {
+    alert(inputText);
+  };
+
+  if (!props.hasOwnProperty("feedback")) {
+    return <>No feedback found!</>;
+  }
+  const feedbackCards = props.feedback.map((feedback, index) => (
+    <FeedbackCard
+      key={index}
+      dateTime={feedback.dateTime}
+      text={feedback.text}
+    />
+  ));
+
   return (
     <>
       <Head>
@@ -40,7 +58,13 @@ export default function Feedback() {
         <Form className="m-2 w-100">
           <Form.Group controlId="">
             <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={inputText}
+              onChange={updateFeedbackInput}
+              placeholder="Enter your feedback!"
+            />
           </Form.Group>
           <Button variant="primary" type="button" onClick={submitFeedback}>
             Submit
@@ -48,14 +72,7 @@ export default function Feedback() {
         </Form>
 
         <Row className="justify-content-center">
-          <Col lg={8}>
-            <FeedbackCard dateTime="2021-01-01" text="Hello world" />
-            <FeedbackCard
-              dateTime="2021-02-19T15:43:00+08:00"
-              text="Hello world again!"
-            />
-            <FeedbackCard />
-          </Col>
+          <Col lg={8}>{feedbackCards}</Col>
         </Row>
       </Container>
     </>
@@ -63,7 +80,47 @@ export default function Feedback() {
 }
 
 export async function getStaticProps(context) {
+  // const res = await fetch("");
+  // const data = await res.json();
+
+  // if (data == null) {
+  //   return {
+  //     props: { },
+  //   };
+  // } else {
+  //   return {
+  //     props: {feedback: data.feedback}
+  //   }
+  // }
+  let feedback = [
+    {
+      dateTime: "2021-02-19T15:43:00Z",
+      text: "The quick brown fox jumps over the lazy dog",
+    },
+    {
+      dateTime: "2021-02-22T15:43:00Z",
+      text: "The quick brown fox jumps over the lazy dog 2",
+    },
+    {
+      dateTime: "2021-02-23T15:43:00Z",
+      text: "The quick brown fox jumps over the lazy dog 3",
+    },
+  ];
+  feedback.sort((a, b) => {
+    const x = dayjs(a);
+    const y = dayjs(b);
+    if (x.isBefore(y)) {
+      return 1;
+    } else if (x.isSame(y)) {
+      return 0;
+    } else {
+      return -1;
+    }
+  });
+
   return {
-    props: {},
+    props: {
+      feedback,
+    },
   };
 }
