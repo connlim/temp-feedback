faca4a1ead7be9e88382eb1a1acba6f805c3724e
Connor Lim <dev@connorlim.net>
2021-03-26 20:59:49 +0800

    Integrate feedback page with API

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,65 +1,84 @@
 import Feedback from "../views/feedback";
 import Welcome from "../views/welcome";
 import dayjs from "dayjs";
+import axios from "axios";
 
 export default function Index(props) {
   if (props.subdomain === "www") {
     return <Welcome />;
   } else {
-    return <Feedback feedback={props.feedback} />;
+    return (
+      <Feedback
+        subdomain={props.subdomain}
+        subdomainExists={props.subdomainExists}
+        feedback={props.feedback}
+      />
+    );
   }
 }
 
 export async function getServerSideProps(context) {
   const subdomain = context.req.headers.host.split(".")[0];
-  console.log(subdomain);
-  let feedback;
 
   if (subdomain === "www") {
-    feedback = null;
+    return {
+      props: {
+        subdomain,
+        subdomainExists: true,
+      },
+    };
   } else {
-    // const res = await axios.get("");
-
-    // if ("feedback" in res.data) {
-    //   return {
-    //     props: {feedback: data.feedback}
-    //   }
-    // } else {
-    //   return {
-    //     props: { },
-    //   };
-    // }
-    feedback = [
-      {
-        dateTime: "2021-02-19T15:43:00Z",
-        text: "The quick brown fox jumps over the lazy dog",
-      },
-      {
-        dateTime: "2021-02-22T15:43:00Z",
-        text: "The quick brown fox jumps over the lazy dog 2",
-      },
-      {
-        dateTime: "2021-02-23T15:43:00Z",
-        text: "The quick brown fox jumps over the lazy dog 3",
-      },
-    ];
-    feedback.sort((a, b) => {
-      const x = dayjs(a);
-      const y = dayjs(b);
-      if (x.isBefore(y)) {
-        return 1;
-      } else if (x.isSame(y)) {
-        return 0;
+    try {
+      const res = await axios.get(
+        "https://07ncbm8zzg.execute-api.us-east-2.amazonaws.com/GetFeedbackFromSubdomain",
+        { params: { subdomain: subdomain } }
+      );
+      let feedback;
+      if ("feedback" in res.data) {
+        feedback = res.data.feedback;
+        feedback.sort((a, b) => {
+          const x = dayjs(a);
+          const y = dayjs(b);
+          if (x.isBefore(y)) {
+            return 1;
+          } else if (x.isSame(y)) {
+            return 0;
+          } else {
+            return -1;
+          }
+        });
       } else {
-        return -1;
+        feedback = [];
       }
-    });
+      return {
+        props: {
+          subdomain,
+          feedback,
+          subdomainExists: true,
+        },
+      };
+    } catch (err) {
+      return {
+        props: {
+          subdomain,
+          feedback: [],
+          subdomainExists: false,
+        },
+      };
+    }
+    //feedback = [
+    //{
+    //dateTime: "2021-02-19T15:43:00Z",
+    //text: "The quick brown fox jumps over the lazy dog",
+    //},
+    //{
+    //dateTime: "2021-02-22T15:43:00Z",
+    //text: "The quick brown fox jumps over the lazy dog 2",
+    //},
+    //{
+    //dateTime: "2021-02-23T15:43:00Z",
+    //text: "The quick brown fox jumps over the lazy dog 3",
+    //},
+    //];
   }
-
-  return {
-    props: {
-      subdomain,
-      feedback,
-    },
-  };
 }
--- nextjs/views/feedback.js
+++ nextjs/views/feedback.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
@@ -8,7 +7,9 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 dayjs.extend(relativeTime);
 
@@ -26,38 +27,64 @@ function FeedbackCard(props) {
 }
 
 export default function Feedback(props) {
+  const [additionalFeedback, setAdditionalFeedback] = useState([]);
   const [inputText, setInputText] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsSubmitting(false);
+    setInputText("");
+    setAdditionalFeedback([]);
+  }, [props.feedback]);
 
   const updateFeedbackInput = (event) => {
     setInputText(event.target.value);
   };
 
-  const submitFeedback = (event) => {
-    alert(inputText);
+  const submitFeedback = () => {
+    setIsSubmitting(true);
+    axios
+      .post(
+        "https://07ncbm8zzg.execute-api.us-east-2.amazonaws.com/AddFeedback",
+        { subdomain: props.subdomain, feedback: inputText }
+      )
+      .then(function (res) {
+        if (res.status === 200) {
+          setAdditionalFeedback([res.data].concat(additionalFeedback));
+          setIsSubmitting(false);
+          setInputText("");
+        }
+        //router.replace(router.asPath);
+      })
+      .catch(function (err) {
+        console.log(err);
+        setIsSubmitting(false);
+      });
   };
 
   let feedbackCards;
-  if (props.hasOwnProperty("feedback") && props.feedback.length > 0) {
-    feedbackCards = props.feedback.map((feedback, index) => (
-      <FeedbackCard
-        key={index}
-        dateTime={feedback.dateTime}
-        text={feedback.text}
-      />
-    ));
+  if (
+    (props.hasOwnProperty("feedback") && props.feedback.length > 0) ||
+    additionalFeedback.length > 0
+  ) {
+    feedbackCards = additionalFeedback
+      .concat(props.feedback)
+      .map((feedback, index) => (
+        <FeedbackCard
+          key={index}
+          dateTime={feedback.dateTime}
+          text={feedback.text}
+        />
+      ));
   } else {
     feedbackCards = <div>No feedback found</div>;
   }
 
-  return (
-    <>
-      <Head>
-        <title>Feedback</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Container className="py-4">
-        <h1>Ephemeral Feedback</h1>
-
+  let content;
+  if (props.subdomainExists) {
+    content = (
+      <>
         <Form className="m-2 w-100">
           <Form.Group controlId="">
             <Form.Label>Example textarea</Form.Label>
@@ -70,13 +97,35 @@ export default function Feedback(props) {
             />
           </Form.Group>
           <Button variant="primary" type="button" onClick={submitFeedback}>
-            Submit
+            {isSubmitting && (
+              <span
+                className="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>
+            )}
+            {isSubmitting ? " Sending..." : "Submit"}
           </Button>
         </Form>
 
         <Row className="justify-content-center">
           <Col lg={8}>{feedbackCards}</Col>
         </Row>
+      </>
+    );
+  } else {
+    content = <div>No subdomain found</div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Feedback</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container className="py-4">
+        <h1>Ephemeral Feedback</h1>
+        {content}
       </Container>
     </>
   );
